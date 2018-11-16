71475a22f0c297ff85714e16b0b4ef4d56cdab9c
Brent Caulfield <brentcaulfield@gmail.com>
2018-11-16 07:49:49 -0500

    Lecture 242: In the ChooseNextWaypoint class I exposed a protected struct variable of type FBlackboardKeySelector and named it IndexKey. Then in the ExecuteTask function I created a local auto variable called BlackboardComp and set it equal to the BlackboardComponent of the OwnerComp. Then I made a local auto variable called Index and set it equal to the Selected Key Name of the IndexKey of the Int value of the BlackboardComp

--- Source/TestingGrounds/ChooseNextWaypoint.h
+++ Source/TestingGrounds/ChooseNextWaypoint.h
@@ -14,9 +14,11 @@ class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
 	
-public:
-	
+public:	
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
 	
 };
