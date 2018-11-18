c431b9f17367381097c8793fd9289f307bbf7322
Brent Caulfield <brentcaulfield@gmail.com>
2018-11-18 10:34:42 -0500

    Lecture 276: Finishing configuring the player's gun and fixing the Fire action called from the FirstPersonCharacter so that it uses the OnFire function from the Gun reference

--- Source/TestingGrounds/Weapons/Gun.cpp
+++ Source/TestingGrounds/Weapons/Gun.cpp
@@ -42,6 +42,14 @@ void AGun::OnFire()
 	// try and fire a projectile
 	if (ProjectileClass != NULL)
 	{
+		const FRotator SpawnRotation = FP_MuzzleLocation->GetComponentRotation();
+		// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
+		const FVector SpawnLocation = FP_MuzzleLocation->GetComponentLocation();
+
+		//Set Spawn Collision Handling Override
+		FActorSpawnParameters ActorSpawnParams;
+		ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+		
 		UWorld* const World = GetWorld();
 		if (World != NULL)
 		{
@@ -53,16 +61,8 @@ void AGun::OnFire()
 			//}
 			//else
 			//{
-				const FRotator SpawnRotation = FP_MuzzleLocation->GetComponentRotation();
-				// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
-				const FVector SpawnLocation = FP_MuzzleLocation->GetComponentLocation();
-
-				//Set Spawn Collision Handling Override
-				FActorSpawnParameters ActorSpawnParams;
-				ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
-
-				// spawn the projectile at the muzzle
-				World->SpawnActor<ABallProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
+			// spawn the projectile at the muzzle
+			World->SpawnActor<ABallProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
 			//}
 		}
 	}
